dd5c0159d0ae5414c01bd1ec45f311fc6fb8ee22
Tomás Lavez <tomlavez@hotmail.com>
2024-07-06 20:37:25 -0300

    Adicionando nome do usuário ao agente de ajuda e memória ao agente principal

--- agents.py
+++ agents.py
@@ -72,10 +72,12 @@ prompt_helper = ChatPromptTemplate.from_messages(
         (
             "system",
             """
-            Você é um desenvolvedor sênior cujo objetivo é facilitar a integração do novo funcionários da sua empresa, {username}, ajudando-os a se familiarizar rapidamente
+            Você é um desenvolvedor sênior cujo objetivo é facilitar a integração do novo funcionários da sua empresa, ajudando-os a se familiarizar rapidamente
             com a cultura, políticas, programas e ferramentas de trabalho. Para isso, você deve conversar casualmente com o usuário e responder suas dúvidas de
             forma clara e detalhada. As principais ferramentas utilizadas são GitHub, VSCode, Jira e Discord. Também pode usar qualquer outra ferramenta que
             interaja com essas. Considere apenas este escopo, e para qualquer pergunta fora dele, responda com 'Desculpe, não posso responder a essa pergunta'.
+
+            Você está recebendo uma pergunta do funcionario de nome {username}. Sempre que possível chamá-lo pelo nome, para criar uma conexão mais pessoal.
 
             Durante suas interações:
 
@@ -86,7 +88,7 @@ prompt_helper = ChatPromptTemplate.from_messages(
             5. Ofereça recursos adicionais, como links para documentos internos, tutoriais e vídeos, para enriquecer a compreensão do usuário.
             6. Incentive o funcionário a fazer perguntas e a se sentir à vontade para expressar qualquer dúvida ou preocupação.
             7. Não fale sobre outras empresas, mantendo o foco exclusivamente na nossa empresa e nas ferramentas mencionadas.
-            8. Não forneça informações pessoais, seja sobre você, outros funcionários ou qualquer outra pessoa.
+            8. Não forneça informações pessoais, seja sobre você, outros funcionários ou qualquer outra pessoa, a não ser do usuário atual ({username}).
             9. Iniba qualquer discurso de ódio, linguagem inadequada ou ofensiva, promovendo sempre um ambiente respeitoso e inclusivo.
 
             Lembre-se de que sua meta é garantir que o novo funcionário se sinta bem-vindo e bem preparado para iniciar suas atividades na empresa,
@@ -304,6 +306,16 @@ agent_result_verifier = RunnableWithMessageHistory(
     history_messages_key="chat_history",
 )
 
+agent_request_identifier_with_chat_history = RunnableWithMessageHistory(
+    agent_request_identifier,
+    lambda session_id: SQLChatMessageHistory(
+        session_id=session_id, connection_string="sqlite:///user_messages.db"
+    ),
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    username="username",
+)
+
 
 # Funções dos agentes auxiliares
 def verify_response(input: str, response, username) -> str:
@@ -318,7 +330,7 @@ def identify_request(input: str, username) -> str:
     identify_input = (
         f"Verifique a seguinte pergunta considerando o contexto da conversa: '{input}'"
     )
-    identification_result = agent_request_identifier.invoke(
+    identification_result = agent_request_identifier_with_chat_history.invoke(
         {"input": identify_input, "username": username},
         config={"configurable": {"session_id": username}},
     )
@@ -349,7 +361,7 @@ def bot_main(input: str, username):
         if "Resposta válida" in verification_result:
             return response
         elif "Texto revisado" in verification_result:
-            res = verification_result.split("Texto revisado:", 1)
+            res = verification_result.split("Texto revisado: ", 1)
             splitRes = res[1]
             return splitRes
         else:
